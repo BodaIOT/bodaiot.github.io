6057744b34460e0c0bb7f31cb30f98f292a0c845
diemonic1 <diemonic1@gmail.com>
2024-04-15 23:39:05 +0300

    upd

--- RSS.js
+++ RSS.js
@@ -1,5 +1,7 @@
-const RSS_URL = `https://rss.stopgame.ru/rss_news.xml`;
-const RSS_URL2 = `https://www.playground.ru/rss/news.xml`;
+
+
+links = [`https://rss.stopgame.ru/rss_news.xml`, `https://www.playground.ru/rss/news.xml`];
+
 const smallTextSize = 19;
 const normalTextSize = 22;
 const bigTextSize = 25;
@@ -7,60 +9,32 @@ const bigTextSize = 25;
 const smallDescSize = 17;
 const bigDescSize = 20;
 
-$.ajax({
-	url: "https://api.rss2json.com/v1/api.json?rss_url=" + RSS_URL,
-	dataType: "json",
-	success: function (result) {
-		RSS = document.getElementById("RSS");
-		result = result.items;
-		console.log(result);
-		result.forEach(element => {
-			desc = element.description.replace("[…]", "") + "<br>" + element.pubDate;
-			console.log(element);
-			cs = 0;
-			if (element.title.length <= 75){
-				cs = bigTextSize;
-			}
-			if (element.title.length > 75 && element.title.length <= 87){
-				cs = normalTextSize;
-			}
-			if (element.title.length > 87){
-				cs = smallTextSize;
-				console.log("small");
-			}
+j = 1;
 
-			cds = smallDescSize;
-			if (element.description.length <= 225){
-				cds = bigDescSize;
-			}
+links.forEach(link => {
+	$.ajax({
+		url: "https://api.rss2json.com/v1/api.json?rss_url=" + link,
+		dataType: "json",
+		success: function (result) {
+			console.log("RSS" + j);
+			RSS = document.getElementById("RSS" + j);
+			j++;
+			result = result.items;
+			console.log(result);
+			result.forEach(element => {date = element.pubDate;
+				hours = date[11] + date[12];
+				hours = parseInt(hours) + 3;
+				hours = hours + "";
+				date = date.replaceAt(11, hours[0]);
+				date = date.replaceAt(12, hours[1]);
 
-			RSS.innerHTML += `
-			<a target="_blank" href="` + element.link + `" style="none">
-			<div class="wrap wrap--1">
-				<div class="container container--1" style="background: linear-gradient(0deg, rgb(0 0 0 / 92%) 48%, rgb(0 0 0 / 48%) 100%), url(` + element.enclosure.link + `); background-size: cover;">
-				<div class="desc" style="font-size:` + cds + `px;">` + desc + `</div>
-				<div class="title" style="font-size:` + cs + `px;">` + element.title + `</div>
-				</div>
-			</div>
-			</a>
-			`
-		});
+				date = date.replaceAt(4, ".");
+				date = date.replaceAt(7, ".");
+				date = date.slice(0, 16);
+				date = date.slice(0, 10) + " — " + date.slice(10, 16);
 
-		
-	},
-	error: function (error) {
-		  console.log("НЕ УДАЛОСЬ ПРОЧЕСТЬ RSS!!!");
-	}
-});
-$.ajax({
-		  url: "https://api.rss2json.com/v1/api.json?rss_url=" + RSS_URL2,
-		  dataType: "json",
-		  success: function (result) {
-			  RSS = document.getElementById("RSS2");
-			  result = result.items;
-			  console.log(result);
-			  result.forEach(element => {
-				desc = element.description.replace("[…]", "") + "<br>" + element.pubDate;
+	
+				desc = element.description.replace("[…]", "") + "<br>" + date;
 				cs = 0;
 				if (element.title.length <= 75){
 					cs = bigTextSize;
@@ -72,7 +46,7 @@ $.ajax({
 					cs = smallTextSize;
 					console.log("small");
 				}
-			
+	
 				cds = smallDescSize;
 				if (element.description.length <= 225){
 					cds = bigDescSize;
@@ -88,11 +62,18 @@ $.ajax({
 				</div>
 				</a>
 				`
-			  });
+				
+			});
+	
+			
+		},
+		error: function (error) {
+			  console.log("НЕ УДАЛОСЬ ПРОЧЕСТЬ RSS!!!");
+		}
+	});
+	
+});
 
-			  
-		  },
-		  error: function (error) {
-				console.log("НЕ УДАЛОСЬ ПРОЧЕСТЬ RSS!!!");
-		  }
-	  });
+String.prototype.replaceAt = function(index, replacement) {
+	return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
