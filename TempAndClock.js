9fdc4b28aacc60e45644260a3543f8bf7e429225
diemonic1 <diemonic1@gmail.com>
2024-08-21 18:31:18 +0300

    NoTemp

--- TempAndClock.js
+++ TempAndClock.js
@@ -38,10 +38,6 @@ token = "123";
 
       cl1 = document.getElementById("clock");
       cl2 = document.getElementById("clock2");
-      tb = document.getElementById("t");
-      th = document.getElementById("h");
-      tb2 = document.getElementById("t2");
-      th2 = document.getElementById("h2");
       rassvet = document.getElementById("rassvet");
       zakat = document.getElementById("zakat");
 
@@ -120,45 +116,5 @@ token = "123";
         }
 
         cl2.innerHTML = day + "." + month + "." + now.getFullYear();
-        req.open(
-          "GET",
-          "https://blynk.cloud/external/api/get?token=" + token + "&v9",
-          false
-        );
-        req.send(null);
-        req2.open(
-          "GET",
-          "https://blynk.cloud/external/api/get?token=" + token + "&v10",
-          false
-        );
-        req2.send(null);
-        t = req.responseText;
-        h = req2.responseText;
-
-        tb.innerHTML = t;
-        th.innerHTML = h;
-        tb2.innerHTML = t;
-        th2.innerHTML = h;
-
-        if (t < 18) tb.style.color = "#0036ff";
-        else if (t >= 18 && t <= 20) tb.style.color = "#7a96ff";
-        else if (t > 20 && t <= 24) tb.style.color = "#ffffff";
-        else if (t > 24 && t <= 26) tb.style.color = "#ffaf7a";
-        else if (t > 26 && t <= 26.5) tb.style.color = "#ff5f45";
-        else if (t > 26.5) tb.style.color = "#f71010";
-
-        if (h <= 15) th.style.color = "#ff5f45";
-        else if (h > 15 && h <= 40) th.style.color = "#ffffff";
-        else if (h > 40) th.style.color = "#109ef7";
-
-        if (t < 18) tb2.style.color = "#0036ff";
-        else if (t >= 18 && t <= 20) tb2.style.color = "#7a96ff";
-        else if (t > 20 && t <= 24) tb2.style.color = "#ffffff";
-        else if (t > 24 && t <= 26) tb2.style.color = "#ffaf7a";
-        else if (t > 26 && t <= 26.5) tb2.style.color = "#ff5f45";
-        else if (t > 26.5) tb2.style.color = "#f71010";
-
-        if (h <= 15) th2.style.color = "#ff5f45";
-        else if (h > 15 && h <= 40) th2.style.color = "#ffffff";
-        else if (h > 40) th2.style.color = "#109ef7";
+        
       }
