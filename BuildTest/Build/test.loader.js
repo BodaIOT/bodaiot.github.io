465196ec3eeef70ffe81c715dc5665f7aad5134b
diemonic1 <diemonic1@gmail.com>
2025-05-21 16:45:32 +0300

    Test

--- BuildTest/Build/test.loader.js
+++ BuildTest/Build/test.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95091811,
+    wasmFileSize: 95091990,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
