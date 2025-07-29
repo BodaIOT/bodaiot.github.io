adabcf002ddda2ef145d1b4d344ef8fdfeb298aa
diemonic1 <diemonic1@gmail.com>
2025-07-29 11:13:04 +0300

    Create horizontalScrolling.js

---
+++ horizontalScrolling.js
@@ -0,0 +1,9 @@
+const horizontalScrolls = document.querySelectorAll(".horizontal-scroll");
+
+horizontalScrolls.forEach((item) => {
+    item.addEventListener("wheel", function (e) {
+        e.preventDefault();
+        if (e.deltaY > 0) item.scrollLeft += 100;
+        else item.scrollLeft -= 100;
+    });
+});
